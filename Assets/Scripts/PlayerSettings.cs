638d75829e30a245d55cee24fc08104d66a0f9c8
Filip Hausman <filip.hausman@gmail.com>
2020-05-25 17:30:23 +0200

    cleaning up repo

---
+++ Assets/Scripts/PlayerSettings.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "PlayerSettings", menuName = "GDS/PlayerSettings")]
+public class PlayerSettings : ScriptableObject
+{
+    public float speed = 0.0f;
+    public float gravitySpeed = 0.0f;
+    public float dashSpeed = 0.0f;
+}
