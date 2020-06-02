b9adf6fab676fcc5125c5cd768c73b38cf7144ef
Filip Hausman <filip.hausman@gmail.com>
2020-06-02 23:18:34 +0200

    adjusting sinusoidal track of projectile

--- Assets/Scripts/Enemies/Projectile.cs
+++ Assets/Scripts/Enemies/Projectile.cs
@@ -7,6 +7,7 @@ public class Projectile : MonoBehaviour
     [SerializeField]
     private Collider _collider = null;
     private float _elapsedTime = 0.0f;
+    private Vector3 _startingPosition = Vector3.zero;
 
     public Vector3 Dir { get; set; } = Vector3.zero;
 
@@ -30,6 +31,7 @@ public class Projectile : MonoBehaviour
     private void Start()
     {
         _collider.enabled = false;
+        _startingPosition = transform.position;
     }
 
     // Update is called once per frame
@@ -38,8 +40,9 @@ public class Projectile : MonoBehaviour
         var deltaTime = Time.deltaTime;
 
         var horizontalVec = transform.right * Dir.x * _settings.projectileSpeed;
-        var verticalVec = transform.up * Dir.y * Mathf.Sin(_elapsedTime * _settings.frequency) * _settings.magnitude;
-        transform.Translate((horizontalVec + verticalVec) * deltaTime);
+        transform.Translate(horizontalVec * deltaTime);
+        var verticalVec = _startingPosition.y + Mathf.Sin(_elapsedTime * _settings.frequency) * _settings.magnitude;
+        transform.position = new Vector3(transform.position.x, verticalVec, transform.position.z);
 
         _elapsedTime += deltaTime;
         if(_elapsedTime >= 0.1f)
