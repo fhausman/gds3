e8f2b66f7e52582163cd7f25ca2fcde947deba47
Filip Hausman <filip.hausman@gmail.com>
2020-06-04 17:33:44 +0200

    enemies common

---
+++ Assets/Scripts/Enemies/EnemiesCommon.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum EnemyStates
+{
+    Idle,
+    Shooting,
+    Dead
+}
+
+public class EnemyIdle : BaseState
+{
+    EnemyBase enemy;
+
+    public EnemyIdle(EnemyBase e)
+    {
+        enemy = e;
+    }
+
+    public override void onUpdate(float deltaTime)
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(enemy.transform.position, enemy.transform.right, out hit, enemy.Range, LayerMask.GetMask("Player")))
+        {
+            StartShooting(1.0f);
+        }
+        else if (Physics.Raycast(enemy.transform.position, -enemy.transform.right, out hit, enemy.Range, LayerMask.GetMask("Player")))
+        {
+            StartShooting(-1.0f);
+        }
+    }
+
+    private void StartShooting(float facingDir)
+    {
+        enemy.FacingDirection = facingDir;
+        enemy.StateMachine.ChangeState(EnemyStates.Shooting);
+        enemy.SpriteRenderer.flipX = facingDir >= 0.0f;
+
+    }
+}
+
+public class EnemyDead : BaseState
+{
+    EnemyBase enemy;
+
+    public EnemyDead(EnemyBase e)
+    {
+        enemy = e;
+    }
+
+    public override void onInit(params object[] args)
+    {
+        Object.Destroy(enemy.gameObject);
+    }
+}
+
+public class EnemyBase : MonoBehaviour
+{
+    #region Settings
+    [SerializeField]
+    private EnemySettings settings = null;
+    public float ShootRate { get => settings.shootRate; }
+    public float Range { get => settings.range; }
+    #endregion
+
+    [SerializeField]
+    private Projectile projectile = null;
+    public Projectile Projectile { get => projectile; }
+
+    [SerializeField]
+    private SpriteRenderer spriteRenderer = null;
+    public SpriteRenderer SpriteRenderer { get => spriteRenderer; }
+
+    public StateMachine<EnemyStates> StateMachine { get; private set; } = new StateMachine<EnemyStates>();
+    public float FacingDirection { get; set; } = -1.0f;
+    public float TimeBetweenShots { get => 1 / ShootRate; }
+    public int Health { get; set; } = 0;
+
+    protected void Start()
+    {
+        Health = settings.health;
+    }
+
+    protected void Update()
+    {
+        StateMachine.OnUpdate(Time.deltaTime);
+    }
+
+    protected void FixedUpdate()
+    {
+        StateMachine.OnFixedUpdate(Time.deltaTime);
+    }
+
+    protected void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Projectile"))
+        {
+            Health--;
+
+            if (Health <= 0)
+                StateMachine.ChangeState(EnemyStates.Dead);
+        }
+    }
+}
