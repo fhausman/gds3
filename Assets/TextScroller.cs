456d7f5456de3e3e4e1f1b969c5632f8b4521ae4
Filip Hausman <filip.hausman@gmail.com>
2020-07-10 22:05:58 +0200

    textscroller prefab

---
+++ Assets/TextScroller.cs
@@ -0,0 +1,39 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TextScroller : MonoBehaviour
+{
+    [SerializeField]
+    private TextAsset[] plotText = null;
+
+    [SerializeField]
+    private string nextScene = "";
+
+    [SerializeField]
+    private TextMeshProUGUI sceneText = null;
+
+    private int currentIndex = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentIndex = 0;
+        sceneText.text = plotText[currentIndex].text;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("Space"))
+        {
+            if (currentIndex > plotText.Length)
+            {
+                SceneManager.LoadScene(nextScene);
+            }
+
+            currentIndex++;
+            sceneText.text = plotText[currentIndex].text;
+        }
+    }
+}
