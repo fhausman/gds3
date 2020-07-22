b9f22e21a039b3fa70bd861a71f708f7dba482ca
Kamil <kamil.piekarski104@gmail.com>
2020-07-22 07:51:57 +0200

    update_15
    - Level update
    - New Laser Detector
    - Added Timed Lasers

---
+++ Assets/Scripts/Lasers/LaserInterval.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserInterval : MonoBehaviour
+    //Made by Kamil :D
+{
+
+     public GameObject[] lasers;
+    [SerializeField] public float repeatRate = 1.0f;
+   
+    void Start()
+    {
+        InvokeRepeating("EnableRepeat", 1.0f, repeatRate);
+    }
+
+    void EnableRepeat()
+    {
+        foreach(GameObject lasers in lasers)
+        {
+            lasers.GetComponent<LineRenderer>().enabled = !lasers.GetComponent<LineRenderer>().enabled;
+            lasers.GetComponent<LaserSource>().enabled = !lasers.GetComponent<LaserSource>().enabled;
+        }
+        
+        
+    }
+
+   
+}
