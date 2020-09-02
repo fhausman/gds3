767bbe27ac4cd425920d673acfeff31b2eb335dd
Filip Hausman <filip.hausman@gmail.com>
2020-09-02 18:58:46 +0200

    moving platforms

--- Assets/Scripts/Interactables/Elevator.cs
+++ Assets/Scripts/Interactables/Elevator.cs
@@ -4,28 +4,46 @@ using UnityEngine;
 
 public class Elevator : MonoBehaviour
 {
+    private enum Direction
+    {
+        HORIZONTAL,
+        VERTICAL
+    };
+
     [SerializeField]
     private float _moveSpeed = 1.0f;
+
+    [SerializeField]
+    private Direction _direction = Direction.HORIZONTAL;
 
     private Rigidbody _rb = null;
     private Vector3 _dir = Vector3.left;
 
+    private float delta = 0.0f;
+
     void Awake()
     {
+        _dir = _direction == Direction.HORIZONTAL ? Vector3.left : Vector3.up;
+
         _rb = GetComponent<Rigidbody>();
     }
 
     private void Update()
     {
         _rb.velocity = _dir * _moveSpeed;
+
+        delta += Time.deltaTime;
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        var hwdp = LayerMask.GetMask("Ground");
+        if (delta < 0.05f)
+            return;
+
         if (collision.gameObject.layer == LayerMask.NameToLayer("Ground"))
         {
             _dir = -_dir;
+            delta = 0.0f;
         }
     }
 }
