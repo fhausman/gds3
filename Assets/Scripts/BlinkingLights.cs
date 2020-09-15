3703ff8dcc98a86b80f659fce65a111763e3a00b
Filip Hausman <filip.hausman@gmail.com>
2020-09-15 02:44:01 +0200

    blinking lights

---
+++ Assets/Scripts/BlinkingLights.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class BlinkingLights : MonoBehaviour
+{
+    [SerializeField]
+    private Light _light = null;
+
+    [SerializeField]
+    private float _probability = 0.2f;
+
+    [SerializeField]
+    private float _brightIntensity = 100.0f;
+
+    [SerializeField]
+    private float _darkerIntensity = 50.0f;
+    // Update is called once per frame
+    void Update()
+    {
+        var rng = Random.Range(0.0f, 1.0f);
+        if(rng < _probability)
+        {
+            _light.intensity = _darkerIntensity;
+        }
+        else
+        {
+            _light.intensity = _brightIntensity;
+        }
+    }
+}
