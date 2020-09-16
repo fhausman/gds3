013e5cc3e01c42e9277d5f762c083dc8042badde
Filip Hausman <filip.hausman@gmail.com>
2020-09-17 01:15:03 +0200

    menu finished end programmed

---
+++ Assets/MenuController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _mainMenu = null;
+
+    [SerializeField]
+    private GameObject _credits = null;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Level1(Snaps)Alt");
+    }
+
+    public void Credits()
+    {
+        _mainMenu.SetActive(false);
+        _credits.SetActive(true);
+    }
+
+    public void Back()
+    {
+        _mainMenu.SetActive(true);
+        _credits.SetActive(false);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
