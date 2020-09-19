0d5f85006d0893c111143893527d5d811b89b2db
Filip Hausman <filip.hausman@gmail.com>
2020-09-20 00:47:23 +0200

    menu fix

--- Assets/Scripts/DontDestroy.cs
+++ Assets/Scripts/DontDestroy.cs
@@ -4,16 +4,6 @@ public class DontDestroy : MonoBehaviour
 {
     void Awake()
     {
-        GameObject[] objs = GameObject.FindGameObjectsWithTag("AmbientSounds");
-
-        if (objs.Length > 1)
-        {
-            foreach (var obj in objs)
-            {
-                Destroy(obj);
-            }
-        }
-
-        DontDestroyOnLoad(this.gameObject);
+        DontDestroyOnLoad(gameObject);
     }
 }
