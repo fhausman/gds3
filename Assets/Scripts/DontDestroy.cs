e1e05b8c6532c4a2e8b565275d303c75ba940e75
Filip Hausman <filip.hausman@gmail.com>
2020-09-20 00:20:42 +0200

    dont destroy ambient sounds

---
+++ Assets/Scripts/DontDestroy.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class DontDestroy : MonoBehaviour
+{
+    void Awake()
+    {
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("AmbientSounds");
+
+        if (objs.Length > 1)
+        {
+            foreach (var obj in objs)
+            {
+                Destroy(obj);
+            }
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
