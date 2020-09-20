4af55731b039d685bc081efa47d3c47099e409bd
Filip Hausman <filip.hausman@gmail.com>
2020-09-20 16:10:07 +0200

    background sounds

---
+++ Assets/Scripts/BackgroundSounds.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundSounds : MonoBehaviour
+{
+    [SerializeField]
+    private AudioSource _audio;
+
+    [SerializeField]
+    private AudioClip[] _sequence;
+
+    private void OnEnable()
+    {
+        StartCoroutine(PlaySequence());
+    }
+
+    private void OnDisable()
+    {
+        StopAllCoroutines();
+    }
+
+    IEnumerator PlaySequence()
+    {
+        int i = 0;
+
+        while(true)
+        {
+            _audio.PlayOneShot(_sequence[i % _sequence.Length]);
+
+            yield return new WaitForSeconds(8.0f);
+
+            ++i;
+        }
+    }
+}
