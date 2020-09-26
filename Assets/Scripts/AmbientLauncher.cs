9502b0e67710fc47d0c4014c14f4feba58162aca
Filip Hausman <filip.hausman@gmail.com>
2020-09-27 00:53:12 +0200

    launching sound on level1

---
+++ Assets/Scripts/AmbientLauncher.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmbientLauncher : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip _onSound;
+
+    [SerializeField]
+    private AudioSource _audio;
+
+    private GameObject _ambientSounds;
+
+    void Start()
+    {
+        _ambientSounds = GameObject.Find("AmbientSounds");
+    }
+
+    public void EnableAmbients()
+    {
+        foreach(Transform child in _ambientSounds.transform)
+        {
+            child.gameObject.SetActive(true);
+        }
+    }
+
+    public void PlayOnSound()
+    {
+        _audio.PlayOneShot(_onSound);
+    }
+}
