09a6b12ffbf8422c5907450e55fd2f38fe26ac33
Filip Hausman <filip.hausman@gmail.com>
2020-09-27 00:36:18 +0200

    can't go out unitl sfx don't end playing

--- Assets/Scripts/TextScroller.cs
+++ Assets/Scripts/TextScroller.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
@@ -18,10 +19,13 @@ public class TextScroller : MonoBehaviour
     [SerializeField]
     private UnityEvent[] events = null;
 
+    public bool CanLoadNextLevel { get; set; } = true;
+
     private int currentIndex = 0;
     private Fade fade;
     private AsyncOperation sceneLoad;
     private string[] chapters = null;
+
 
     // Start is called before the first frame update
     void Start()
@@ -41,31 +45,44 @@ public class TextScroller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.anyKeyDown)
+        if (Input.anyKeyDown && currentIndex <= chapters.Length - 1)
         {
             if (chapters != null)
             {
                 currentIndex++;
-
-                fade.FadeIn();
                 if (currentIndex > chapters.Length - 1)
                 {
-                    LoadNextScene();
+                    StartCoroutine(LoadNextScene());
                     return;
                 }
-
-                sceneText.text = chapters[currentIndex];
-                events[currentIndex].Invoke();
+                else
+                {
+                    fade.FadeIn();
+                    sceneText.text = chapters[currentIndex];
+                    events[currentIndex].Invoke();
+                }
             }
             else
             {
-                LoadNextScene();
+                StartCoroutine(LoadNextScene());
             }
         }
     }
 
-    void LoadNextScene()
+    void AllowLoad()
     {
+        CanLoadNextLevel = true;
+    }
+
+    void DisallowLoad()
+    {
+        CanLoadNextLevel = false;
+    }
+
+    IEnumerator LoadNextScene()
+    {
+        yield return new WaitUntil(() => CanLoadNextLevel == true);
+
         fade.onFadeOutEnd.AddListener(() => sceneLoad.allowSceneActivation = true);
         fade.FadeOut();
         gameObject.SetActive(false);
