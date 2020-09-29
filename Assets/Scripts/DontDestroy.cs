8f39ba5c2a91c99085dbd040e496bc3b74be9be2
Filip Hausman <filip.hausman@gmail.com>
2020-09-29 23:08:48 +0200

    don't create two ambient sounds if menu is reloaded

--- Assets/Scripts/DontDestroy.cs
+++ Assets/Scripts/DontDestroy.cs
@@ -4,6 +4,14 @@ public class DontDestroy : MonoBehaviour
 {
     void Awake()
     {
+        var tag = gameObject.tag;
+        var others = GameObject.FindGameObjectsWithTag(tag);
+        if(others.Length > 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
         DontDestroyOnLoad(gameObject);
     }
 }
